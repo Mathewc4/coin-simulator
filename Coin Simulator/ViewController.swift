ee78c6e6c1d82995901c8075e66778fa2d5ccbee
Your Name <you@example.com>
2019-01-18 13:41:19 -0700

    Initial Commit

---
+++ Coin Simulator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coin Simulator
+//
+//  Created by Mathew Cheung on 2019-01-18.
+//  Copyright © 2019 Mathew Cheung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
